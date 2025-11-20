413bdeeb93a037479f97225ca916a2a61c7fa9f7
Tola <agapebondservant@yahoo.com>
2025-11-20 18:31:42 +0000

    updates

--- notebooks/graphrag.sh
+++ notebooks/graphrag.sh
@@ -1,4 +1,4 @@
 cp settings.yaml $1/settings.yaml
-python -m graphrag init --root $1 &2>1
+python -m graphrag init --root $1 2>&1
 python -m graphrag prompt-tune --root $1 --config $2 --no-discover-entity-types --domain "code generation" 2>&1
 python -m graphrag index --method fast --root $1 2>&1
