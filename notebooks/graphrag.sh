618b1cd396a26e866ea562380d2ed64682203eec
Tola <agapebondservant@yahoo.com>
2025-11-28 16:58:49 +0000

    updates

--- notebooks/graphrag.sh
+++ notebooks/graphrag.sh
@@ -1,11 +1,12 @@
-echo "Copying settings.yaml..."
-# cp settings.yaml $1/settings.yaml 2>&1
-
 echo "Initializing GraphRAG index..."
-# python -m graphrag init --root $1 2>&1
+python -m graphrag init --force --root $1 2>&1
+
+echo "Copying settings.yaml..."
+cp settings.yaml $1/settings.yaml
+sleep 5
 
 echo "Configuring prompts..."
-python -m graphrag prompt-tune --root $1 --config $2 --no-discover-entity-types --domain "code generation" 2>&1
+python -m graphrag prompt-tune --root $1 --config $2 --no-discover-entity-types --output $1/prompts --domain "code generation" 2>&1
 
 echo "Populating GraphRAG index..."
-python -m graphrag index --method fast --root $1 2>&1
+python -m graphrag index --root $1 2>&1
